a68eb2d6304f8b8c62ce2a7b1f98076f0d24b4bd
rishabhsanmani <rishabhsanmani96@gmail.com>
2026-05-05 15:24:16 +0530

    React JS Router

---
+++ router/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+
+
+function App(){
+  return(
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
